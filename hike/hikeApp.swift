71c58da8cebf9544a85c589c6614764ac7ebf9c9
Rodrigo Silva <rodrigoa.work@gmail.com>
2023-09-29 20:07:40 -0300

    Initial Commit

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Rodrigo Silva on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
